c7b5ca58f254d4eef305f162309bec3e59f962be
Patrick Dobler <patrick.dobler@gmail.com>
2022-08-14 22:00:45 +0200

    added support for arduino F macro

--- simple_logger.cpp
+++ simple_logger.cpp
@@ -8,44 +8,65 @@
 namespace logger
 {
 
-Level log_level = None;
+  Level log_level = None;
 
-HardwareSerial serialOut = NULL;
+  HardwareSerial serialOut = NULL;
 
-void set_serial(HardwareSerial &serial) {
-  serialOut = serial;
-}
+  char logBuf[MAX_MESSAGE_LENGTH];
 
-void set_level(Level level) {
-  log_level = level;
-}
-
-void serialPrintf(char const *level_name, char const *message) {
-  if (serialOut != NULL)
-    serialOut.printf("%s: %s\n", level_name, message);
-}
-
-static void log(char const *level_name, char const *fmt, va_list as)
-{
-  char buf[MAX_MESSAGE_LENGTH];
-  vsnprintf(buf, MAX_MESSAGE_LENGTH, fmt, as);
-  serialPrintf(level_name, buf);
-}
-
-#define DEFINE_LOGGING_FUNCTION(name, level) \
-  void name(char const *fmt, ...)          \
-  {                                        \
-    if(log_level < level) return;        \
-    \
-    va_list as;                          \
-    va_start(as, fmt);                   \
-    log(#name, fmt, as);                 \
-    va_end(as);                          \
+  void set_serial(HardwareSerial &serial)
+  {
+    serialOut = serial;
   }
 
-DEFINE_LOGGING_FUNCTION(err, Error);
-DEFINE_LOGGING_FUNCTION(warn, Warning);
-DEFINE_LOGGING_FUNCTION(info, Info);
-DEFINE_LOGGING_FUNCTION(debug, Debug);
+  void set_level(Level level)
+  {
+    log_level = level;
+  }
 
+  void serialPrintf(const char *level_name, const char *message)
+  {
+    if (serialOut != NULL)
+      serialOut.printf("%s: %s\n", level_name, message);
+  }
+
+  static void log(const char *level_name, const char *fmt, va_list as)
+  {
+    vsnprintf(logBuf, MAX_MESSAGE_LENGTH, fmt, as);
+    serialPrintf(level_name, logBuf);
+  }
+
+#define DEFINE_LOGGING_FUNCTION(name, level) \
+  void name(const char *fmt, ...)            \
+  {                                          \
+    if (log_level < level)                   \
+      return;                                \
+                                             \
+    va_list as;                              \
+    va_start(as, fmt);                       \
+    log(#name, fmt, as);                     \
+    va_end(as);                              \
+  }
+
+#define DEFINE_LOGGING_FUNCTION2(name, level)       \
+  void name(const __FlashStringHelper *fmt, ...) \
+  {                                                 \
+    if (log_level < level)                          \
+      return;                                       \
+                                                    \
+    va_list as;                                     \
+    va_start(as, fmt);                           \
+    log(#name, (PGM_P)(fmt), as);                \
+    va_end(as);                                     \
+  }
+
+  DEFINE_LOGGING_FUNCTION(err, Error);
+  DEFINE_LOGGING_FUNCTION(warn, Warning);
+  DEFINE_LOGGING_FUNCTION(info, Info);
+  DEFINE_LOGGING_FUNCTION(debug, Debug);
+
+  DEFINE_LOGGING_FUNCTION2(err, Error);
+  DEFINE_LOGGING_FUNCTION2(warn, Warning);
+  DEFINE_LOGGING_FUNCTION2(info, Info);
+  DEFINE_LOGGING_FUNCTION2(debug, Debug);
 }
--- simple_logger.h
+++ simple_logger.h
@@ -3,28 +3,34 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 
-size_t const MAX_MESSAGE_LENGTH = 256;
+size_t const MAX_MESSAGE_LENGTH = 64;
 
 namespace logger
 {
-  
-enum Level
-{
-  None,
-  Error,
-  Warning,
-  Info,
-  Debug,
-};
 
-void set_serial(HardwareSerial &serial);
+  enum Level
+  {
+    None,
+    Error,
+    Warning,
+    Info,
+    Debug,
+  };
 
-void set_level(Level level);
+  void set_serial(HardwareSerial &serial);
 
-void err(char const *fmt, ...);
-void warn(char const *fmt, ...);
-void info(char const *fmt, ...);
-void debug(char const *fmt, ...);
+  void set_level(Level level);
+
+  void err(const char *fmt, ...);
+  void warn(const char *fmt, ...);
+  void info(const char *fmt, ...);
+  void debug(const char *fmt, ...);
+
+  void err(const __FlashStringHelper *fmt, ...);
+  void warn(const __FlashStringHelper *fmt, ...);
+  void info(const __FlashStringHelper *fmt, ...);
+  void debug(const __FlashStringHelper *fmt, ...);
+
 }
 
 #endif
